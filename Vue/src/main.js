85075c7c27e47ae8a273f58284b436cfa657a997
Betty <1942433767@qq.com>
2020-09-22 15:58:15 +0800

    feat(vue): add vue-mqtt example

---
+++ Vue/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
