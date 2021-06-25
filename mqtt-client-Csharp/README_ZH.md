7fb1ab112025e86a978a6739a37bc77749b22eb0
wuzufeng <704220980@qq.com>
2021-06-25 16:43:07 +0800

    feat(Csharp):add Csharp mqtt example

---
+++ mqtt-client-Csharp/README_ZH.md
@@ -0,0 +1,10 @@
+# C# MQTT 客户端使用示例
+在本示例中我们提供了 C# 订阅主题和发送信息的示例代码。
+有关 paho.mqtt.m2mqtt 客户端更多使用文档，请参阅 [paho.mqtt.m2mqtt 官方文档](https://www.eclipse.org/paho/index.php?page=clients/dotnet/index.php)。
+
+
+## 运行
+在publish或者subscribe目录下执行以下命令
+```bash
+dotnet run
+```
---
+++ mqtt-client-Csharp/publish/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace publish
+{
+    class Program
+    {
+        static MqttClient ConnectMQTT(string broker, int port, string clientId)
+        {
+            MqttClient client = new MqttClient(broker, port, false, MqttSslProtocols.None, null, null);
+            client.Connect(clientId);
+            if (client.IsConnected)
+            {
+                Console.WriteLine("Connected to MQTT Broker");
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect");
+            }
+            return client;
+        }
+        static void Publish(MqttClient client, string topic)
+        {
+            int msg_count = 0;
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1 * 1000);
+                string msg = "messages: " + msg_count.ToString();
+                client.Publish(topic, System.Text.Encoding.UTF8.GetBytes(msg));
+                Console.WriteLine("Send `{0}` to topic `{1}`", msg, topic);
+                msg_count++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string broker = "broker.emqx.io";
+            int port = 1883;
+            string topic = "Csharp/mqtt";
+            string clientId = Guid.NewGuid().ToString();
+            MqttClient client = ConnectMQTT(broker, port, clientId);
+            Publish(client, topic);
+        }
+    }
+}
