f67cbcf8874a91a6a8203245091a80fcbc8b736e
wuzufeng <704220980@qq.com>
2021-06-25 16:43:07 +0800

    feat(Csharp):add Csharp mqtt example

---
+++ mqtt-client-Csharp/README.md
@@ -0,0 +1,10 @@
+# C# MQTT client examples
+In this example we provide example code for C# to subscribe to topics and send messages.
+For more documentation on the use of the paho.mqtt.m2mqtt client, see the [paho.mqtt.m2mqtt - documentation](https://www.eclipse.org/paho/index.php?page=clients/dotnet/index.php)
+
+
+## Run
+Execute the following command in the publish or subscribe directory
+```bash
+dotnet run
+```
---
+++ mqtt-client-Csharp/subscribe/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace subscribe
+{
+    class Program
+    {
+        static MqttClient ConnectMQTT(string broker, int port, string clientId)
+        {
+            MqttClient client = new MqttClient(broker, port, false, MqttSslProtocols.None, null, null);
+            client.Connect(clientId);
+            if (client.IsConnected)
+            {
+                Console.WriteLine("Connected to MQTT Broker");
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect");
+            }
+            return client;
+        }
+        static void Subscribe(MqttClient client, string topic)
+        {
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+        }
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string payload = System.Text.Encoding.Default.GetString(e.Message);
+            Console.WriteLine("Received `{0}` from `{1}` topic", payload, e.Topic.ToString());
+        }
+        static void Main(string[] args)
+        {
+            string broker = "broker.emqx.io";
+            int port = 1883;
+            string topic = "Csharp/mqtt";
+            string clientId = Guid.NewGuid().ToString();
+            MqttClient client = ConnectMQTT(broker, port, clientId);
+            Subscribe(client, topic);
+        }
+    }
+}
