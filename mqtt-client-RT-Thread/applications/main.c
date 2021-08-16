c69da759b177b47c50899b156fec6188f3ed2566
alvin1221 <68314044+alvin1221@users.noreply.github.com>
2021-08-17 00:31:10 +0800

    add RT-Thread example (#22)
    * Add mqtt-client-RT-Thread example project

---
+++ mqtt-client-RT-Thread/applications/main.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2006-2020, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020-09-02     RT-Thread    first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include "drv_common.h"
+#include "mqtt_client.h"
+
+#define LED_PIN GET_PIN(I, 8)
+
+#define WIFI_SSID "LINKSYS_EMQX_IOT"
+#define WIFI_PASSWORD "emqx1025"
+
+extern void wlan_autoconnect_init(void);
+
+int main(void)
+{
+    rt_uint32_t count = 1;
+    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
+
+    rt_wlan_connect(WIFI_SSID, WIFI_PASSWORD);
+
+    /* init Wi-Fi auto connect feature */
+    wlan_autoconnect_init();
+    /* enable auto reconnect on WLAN device */
+    rt_wlan_config_autoreconnect(RT_TRUE);
+
+    rt_thread_mdelay(5000);
+    mqtt_client_start();
+
+    while (count++)
+    {
+        rt_thread_mdelay(500);
+        rt_pin_write(LED_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED_PIN, PIN_LOW);
+    }
+
+    mqtt_client_stop();
+    return RT_EOK;
+}
+
+#include "stm32h7xx.h"
+static int vtor_config(void)
+{
+    /* Vector Table Relocation in Internal QSPI_FLASH */
+    SCB->VTOR = QSPI_BASE;
+    return 0;
+}
+INIT_BOARD_EXPORT(vtor_config);
+
---
+++ mqtt-client-RT-Thread/applications/mqtt_client.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-08-10     Hongwei Wang       the first version
+ */
+#ifndef APPLICATIONS_MQTT_CLIENT_H_
+#define APPLICATIONS_MQTT_CLIENT_H_
+
+extern void mqtt_client_start(void);
+extern void mqtt_client_stop(void);
+
+#endif /* APPLICATIONS_MQTT_CLIENT_H_ */
