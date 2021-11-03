bf3a07831a19cceaf5db366aff010c6b3cd1ee8b
lee-emqx <lihj@emqx.io>
2021-11-03 18:52:43 +0800

    * UPDATE [qt] update qt demo

--- mqtt-client-Qt/example.cpp
+++ mqtt-client-Qt/example.cpp
@@ -34,153 +34,203 @@
 #include <QTimer>
 #include <iostream>
 
-using namespace std;
-
-
-int fn (int i ) {
-
-        cout<< "dadasdasd" << endl;
-    return i*i;
-}
-
-static int i = fn(8);
-// broker 地址.
-const QHostAddress EXAMPLE_HOST = QHostAddress::Null;
-// const QHostAddress EXAMPLE_HOST = QHostAddress::LocalHost;
+// broker 地址, QHostAddress 除了localhost 和 null 等特殊情况，支持 ip 方式。
+// const QHostAddress EXAMPLE_HOST = QHostAddress::Null;
+const QHostAddress EXAMPLE_HOST = QHostAddress::LocalHost;
 // const QHostAddress EXAMPLE_HOST = QHostAddress("119.23.236.219");
 // broker 端口.
 const quint16 EXAMPLE_PORT = 1883;
 // 订阅/发布的主题
-const QString EXAMPLE_TOPIC = "qmqtt/exampletopic";
+const QString EXAMPLE_TOPIC = "qmqtt/example/topic";
 
 class Publisher : public QMQTT::Client
 {
     Q_OBJECT
-public:
-    // 构造发布客户端
-    explicit Publisher(const QHostAddress& host = EXAMPLE_HOST,
-                       const quint16 port = EXAMPLE_PORT,
-                       QObject* parent = NULL)
-        : QMQTT::Client(host, port, parent)
-        , _number(0)
-    {
-        // 连接信号和槽，连接建立的响应和超时响应
-        connect(this, &Publisher::connected, this, &Publisher::onConnected);
-        connect(&_timer, &QTimer::timeout, this, &Publisher::onTimeout);
-        // connect(this, &Publisher::disconnected, this, &Publisher::onDisconnected);
-    }
-    virtual ~Publisher() {}
-
-    QTimer _timer;
-    quint16 _number;
-
-public slots:
-    void onConnected()
-    {
-        subscribe(EXAMPLE_TOPIC, 0);
-        _timer.start(1000);
-    }
-
-    // 定时发布消息, 消息发布100条以后关闭
-    void onTimeout()
-    {
-        QMQTT::Message message(_number, EXAMPLE_TOPIC,
-                               QString("Number is %1").arg(_number).toUtf8());
-        message.setQos(2);
-        publish(message);
-        _number++;
-        if(_number >= 100)
+    public:
+        // 构造发布客户端
+        explicit Publisher(const QHostAddress& host = EXAMPLE_HOST,
+                const quint16 port = EXAMPLE_PORT,
+                QObject* parent = NULL)
+            : QMQTT::Client(host, port, parent)
+              , _number(0)
+              , _qout(stdout)
         {
-            _timer.stop();
-            disconnectFromHost();
+            // 关联连接的信号和槽函数
+            connect(this, &Publisher::connected, this, &Publisher::onConnected);
+            // 关联定时器和消息发布槽函数
+            connect(&_timer, &QTimer::timeout, this, &Publisher::onTimeout);
+            connect(this, &Publisher::received, this, &Publisher::onReceived);
+            connect(this, &Publisher::subscribed, this, &Publisher::onSubscribed);
+
+            // 关联断开的信号和槽函数
+            connect(this, &Publisher::disconnected, this, &Publisher::onDisconnected);
+        }
+        virtual ~Publisher() {}
+
+        QTimer _timer;
+        quint16 _number;
+        QTextStream _qout;
+
+    public slots:
+        void onConnected()
+        {
+            subscribe("will/topic", 0);
+            // 启动或重启定时器，毫秒精度。
+            _timer.start(1000);
+        }
+
+        // 定时发布消息, 消息发布100条以后关闭
+        void onTimeout()
+        {
+            // QMQTT::Message message(_number, EXAMPLE_TOPIC,
+            //         QString("Number is %1").arg(_number).toUtf8());
+            QMQTT::Message message(_number, EXAMPLE_TOPIC,
+                    QString("Number is %1").arg(_number).toUtf8());
+            message.setQos(0);
+            // publish(message);
+
+            std::cout << publish(message) << std::endl;
+            _number++;
+
+            if(_number >= 1000)
+            {
+                _timer.stop();
+                disconnectFromHost();
+                QTimer::singleShot(0, qApp, &QCoreApplication::quit);
+            }
+        }
+
+
+        void onSubscribed(const QString& topic)
+        {
+            _qout << "subscribed " << topic << endl;
+        }
+
+
+        void onReceived(const QMQTT::Message& message)
+        {
+            _qout << "Received from topic: \"" << message.topic()
+                << "\"" << endl;
+            _qout << "Received payload: \"" << QString::fromUtf8(message.payload())
+                << "\"" << endl;
+        }
+
+        void onDisconnected()
+        {
             QTimer::singleShot(0, qApp, &QCoreApplication::quit);
         }
-    }
-
-    void onDisconnected()
-    {
-        QTimer::singleShot(0, qApp, &QCoreApplication::quit);
-    }
 };
 
 class Subscriber : public QMQTT::Client
 {
     Q_OBJECT
-public:
-    // 构造订阅客户端
-    explicit Subscriber(const QHostAddress& host = EXAMPLE_HOST,
-                        const quint16 port = EXAMPLE_PORT,
-                        QObject* parent = NULL)
-        : QMQTT::Client(host, port, parent)
-        , _qout(stdout)
-    {
-        // 连接信号和槽，连接建立、收到订阅的主题及消息的相应。
-        connect(this, &Subscriber::connected, this, &Subscriber::onConnected);
-        connect(this, &Subscriber::subscribed, this, &Subscriber::onSubscribed);
-        connect(this, &Subscriber::received, this, &Subscriber::onReceived);
-        // connect(this, &Subscriber::disconnected, this, &Subscriber::onConnected);
-    }
-    virtual ~Subscriber() {}
+    public:
+        // 构造订阅客户端
+        explicit Subscriber(const QHostAddress& host = EXAMPLE_HOST,
+                const quint16 port = EXAMPLE_PORT,
+                QObject* parent = NULL)
+            : QMQTT::Client(host, port, parent)
+              , _qout(stdout)
+        {
+            // 连接信号和槽，连接建立、收到订阅的主题及消息的相应。
+            connect(this, &Subscriber::connected, this, &Subscriber::onConnected);
+            connect(this, &Subscriber::subscribed, this, &Subscriber::onSubscribed);
+            connect(this, &Subscriber::received, this, &Subscriber::onReceived);
+        }
+        virtual ~Subscriber() {}
 
-    QTextStream _qout;
+        QTextStream _qout;
 
-public slots:
-    void onConnected()
-    {
-        _qout << "connected" << endl;
-        subscribe(EXAMPLE_TOPIC, 0);
-    }
+    public slots:
+        // 连接成功是处理函数
+        void onConnected()
+        {
+            _qout << "connected" << endl;
+            subscribe(EXAMPLE_TOPIC, 0);
+        }
 
-    void onSubscribed(const QString& topic)
-    {
-        _qout << "subscribed " << topic << endl;
-    }
+        // 订阅成功的回调
+        void onSubscribed(const QString& topic)
+        {
+            _qout << "subscribed " << topic << endl;
+        }
 
-    void onReceived(const QMQTT::Message& message)
-    {
-        _qout << "publish received: \"" << QString::fromUtf8(message.payload())
-              << "\"" << endl;
-    }
+        // 收到消息的回调
+        void onReceived(const QMQTT::Message& message)
+        {
+            _qout << "Received from topic: \"" << message.topic()
+                << "\"" << endl;
+            _qout << "Received payload: \"" << QString::fromUtf8(message.payload())
+                << "\"" << endl;
+        }
 };
 
 
 int main(int argc, char** argv)
 {
-    cout<< "begin main" << endl;
+    if (argc != 2) {
+        std::cout << "Usage: qmqtt_example [pub | sub]" << std::endl;
+        return -1;
+    }
+
+    QString s = QString(argv[1]);
+    if (!s.contains("sub") && !s.contains("pub")) {
+
+        QTextStream qout(stdout);
+        qout<< "Unknown argument: " << s << endl;
+        std::cout << "Usage: qmqtt_example [pub | sub]" << std::endl;
+        return -1;
+    }
+
+
+    // 定义一个 Qt 应用程序对象, Qt 图形界面程序的入口
     QCoreApplication app(argc, argv);
     Subscriber subscriber;
-    
-    // 设置client id
-    // subscriber.setHostName("119.23.236.219");
-    subscriber.setHostName("iot-platform.cloud");
-    subscriber.setVersion(QMQTT::V3_1_1);
-    subscriber.setClientId("sub");
-    // 设置用户名
-    subscriber.setUsername("alvin");
-    // 设置密码
-    subscriber.setPassword("HHH0000");
-    // 设置是否自动重连
-    subscriber.setAutoReconnect(true);
-    // 设置重连间隔
-    subscriber.setAutoReconnectInterval(100);
-    // 设置保活时间间隔
-    subscriber.setKeepAlive(100);
-    // 设置清空会话
-    subscriber.setCleanSession(true);
-    // 连接到broker
-    subscriber.connectToHost();
-
     Publisher publisher;
-    publisher.setHostName("119.23.236.219");
-    // publisher.setHostName("iot-platform.cloud");
-    publisher.setClientId("pub");
-    publisher.setUsername("alvin");
-    publisher.setPassword("HHH0000");
-    publisher.setAutoReconnect(true);
-    publisher.setAutoReconnectInterval(100);
-    publisher.setKeepAlive(100);
-    publisher.connectToHost();
 
+    if (s.contains("sub")) {
+       std::cout << "sub" << std::endl;
+
+       // 设置 Hostname， 这里支持ip和域名两种方式。
+       // subscriber.setHostName("34.211.84.46");
+       // subscriber.setHostName("broker.emqx.io");
+       // 设置 mqtt 版本, 支持 V3_1_1 和 V3_1_0。
+       subscriber.setVersion(QMQTT::V3_1_1);
+       // 设置 client id
+       subscriber.setClientId("sub_client");
+       // 设置用户名
+       subscriber.setUsername("username");
+       // 设置密码
+       subscriber.setPassword("password");
+       // 设置是否自动重连
+       subscriber.setAutoReconnect(true);
+       // 设置重连间隔
+       subscriber.setAutoReconnectInterval(100);
+       // 设置保活时间间隔
+       subscriber.setKeepAlive(100);
+       // 设置清空会话
+       subscriber.setCleanSession(false);
+       // 设置遗愿主题
+       subscriber.setWillTopic("will/topic");
+
+       QByteArray ba("Hello");
+       // 设置遗愿消息
+       subscriber.setWillMessage(ba);
+       // 连接到broker
+       subscriber.connectToHost();
+    } else if (s.contains("pub")) {
+
+        // publisher.setHostName("broker.emqx.io");
+        publisher.setClientId("pub_client11");
+        publisher.setUsername("username");
+        publisher.setPassword("password");
+        publisher.setAutoReconnect(true);
+        publisher.setAutoReconnectInterval(100);
+        publisher.setKeepAlive(100);
+        publisher.connectToHost();
+    }
+
+    // exec() 函数会进入 Qt 应用程序的事件循环函数等待事件触发。
     return app.exec();
 }
 
