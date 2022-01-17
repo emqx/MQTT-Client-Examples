c89f38a34907ac2039696658265cd6548be0071b
alvin1221 <alvin@emqx.io>
2022-01-17 16:48:47 +0800

    Update [Csharp-MQTTnet] support tcp/tls/ws/wss connection

--- mqtt-client-Csharp-MqttNet/mqtt-client-Csharp-MqttNet/Program.cs
+++ mqtt-client-Csharp-MqttNet/mqtt-client-Csharp-MqttNet/Program.cs
@@ -11,48 +11,123 @@ namespace mqttclientCsharpMqttNet
 {
     class MainClass
     {
+        private static ManagedMqttClientOptions TcpMqttClientOptions(string url)
+        {
+            return new ManagedMqttClientOptionsBuilder()
+                .WithClientOptions(
+                    new MqttClientOptionsBuilder()
+                        .WithClientId("EMQX_" + Guid.NewGuid().ToString())
+                        .WithTcpServer(url)
+                        //.WithCredentials("user", "pass")
+                        .WithCleanSession()
+                        .Build()
+                )
+                .Build();
+        }
+
+        private static ManagedMqttClientOptions TlsMqttClientOptions(string url, string caFile)
+        {
+            return new ManagedMqttClientOptionsBuilder()
+                .WithClientOptions(
+                    new MqttClientOptionsBuilder()
+                        .WithClientId("EMQX_" + Guid.NewGuid().ToString())
+                        .WithTcpServer(url)
+                        //.WithCredentials("user", "pass")
+                        .WithCleanSession()
+                        .WithTls(
+                            new MqttClientOptionsBuilderTlsParameters()
+                            {
+                                UseTls = true,
+                                SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
+                                Certificates = new List<X509Certificate>()
+                                {
+                                    // Download from https://www.emqx.com/en/mqtt/public-mqtt5-broker
+                                    X509Certificate.CreateFromCertFile(caFile)
+                                }
+                            }
+                        )
+                        .Build()
+                )
+                .Build();
+        }
+
+        private static ManagedMqttClientOptions WsMqttClientOptions(string url)
+        {
+            return new ManagedMqttClientOptionsBuilder()
+                .WithClientOptions(
+                    new MqttClientOptionsBuilder()
+                        .WithClientId("EMQX_" + Guid.NewGuid().ToString())
+                        .WithWebSocketServer(url)
+                        //.WithCredentials("user", "pass")
+                        .WithCleanSession()
+                        .Build()
+                )
+                .Build();
+        }
+
+        private static ManagedMqttClientOptions WssMqttClientOptions(string url, string caFile)
+        {
+            return new ManagedMqttClientOptionsBuilder()
+                .WithClientOptions(
+                    new MqttClientOptionsBuilder()
+                        .WithClientId("EMQX_" + Guid.NewGuid().ToString())
+                        .WithWebSocketServer(url)
+                        //.WithCredentials("user", "pass")
+                        .WithCleanSession()
+                        .WithTls(
+                            new MqttClientOptionsBuilderTlsParameters()
+                            {
+                                UseTls = true,
+                                SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
+                                Certificates = new List<X509Certificate>()
+                                {
+                                    // Download from https://www.emqx.com/en/mqtt/public-mqtt5-broker
+                                    X509Certificate.CreateFromCertFile(caFile)
+                                }
+                            }
+                        )
+                        .Build()
+                )
+                .Build();
+        }
+
         public static async Task Main(string[] args)
         {
-            bool useTls = false;
             string topic = "/emqx/topic/msg";
-            string caFile = @"broker.emqx.io-ca.crt";
-            string broker = "broker.emqx.io";
+            string connectionType = "tcp"; //tcp, tls, ws, wss
+            ManagedMqttClientOptions options = null;
 
-            var options = useTls
-                ? new ManagedMqttClientOptionsBuilder()
-                  .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
-                  .WithClientOptions(
-                      new MqttClientOptionsBuilder()
-                          .WithClientId("EMQX_" + Guid.NewGuid().ToString())
-                          .WithTcpServer(broker)
-                          //.WithCredentials("user", "pass")
-                          .WithCleanSession()
-                          .WithTls(
-                              new MqttClientOptionsBuilderTlsParameters()
-                              {
-                                  UseTls = true,
-                                  SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
-                                  Certificates = new List<X509Certificate>()
-                                  {
-                                      // Download from https://www.emqx.com/en/mqtt/public-mqtt5-broker
-                                      X509Certificate.CreateFromCertFile(caFile)
-                                  }
-                              }
-                          )
-                          .Build()
-                  )
-                  .Build()
-                : new ManagedMqttClientOptionsBuilder()
-                  .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
-                  .WithClientOptions(
-                      new MqttClientOptionsBuilder()
-                          .WithClientId("EMQX_" + Guid.NewGuid().ToString())
-                          .WithTcpServer(broker)
-                          //.WithCredentials("user", "pass")
-                          .WithCleanSession()
-                          .Build()
-                  )
-                  .Build();
+            if (args.Length >= 1)
+            {
+                connectionType = args[0];
+                Console.WriteLine("connection: {0}", connectionType);
+            }
+
+            switch (connectionType)
+            {
+                case "tcp":
+                    options = TcpMqttClientOptions("broker.emqx.io");
+                    Console.WriteLine("tcp connection");
+                    break;
+                case "tls":
+                    options = TlsMqttClientOptions("broker.emqx.io", @"broker.emqx.io-ca.crt");
+                    Console.WriteLine("tls connection");
+                    break;
+                case "ws":
+                    options = WsMqttClientOptions("broker.emqx.io:8083/mqtt");
+                    Console.WriteLine("ws connection");
+                    break;
+                case "wss":
+                    options = WssMqttClientOptions(
+                        "broker.emqx.io:8084/mqtt",
+                        @"broker.emqx.io-ca.crt"
+                    );
+                    Console.WriteLine("wss connection");
+                    break;
+                default:
+                    Console.WriteLine("Unsupported: {0}" + args[1]);
+                    return;
+            }
 
             var mqttClient = new MqttFactory().CreateManagedMqttClient();
 
@@ -64,6 +139,10 @@ namespace mqttclientCsharpMqttNet
                         System.Text.Encoding.UTF8.GetString(msg.ApplicationMessage.Payload),
                         msg.ApplicationMessage.Topic
                     )
+            );
+
+            mqttClient.UseConnectedHandler(
+                (arg) => Console.WriteLine("Establish connection " + arg.ConnectResult.ResultCode)
             );
 
             await mqttClient.StartAsync(options);
