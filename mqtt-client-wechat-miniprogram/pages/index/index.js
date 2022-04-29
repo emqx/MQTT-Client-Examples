3bdf32d4dcbee80042ebefe2c0a87e1c42490989
0721Betty <1942433767@qq.com>
2022-04-29 18:20:29 +0800

    refactor(wechat): modify wechat miniprogram mqtt demo

--- mqtt-client-wechat-miniprogram/pages/index/index.js
+++ mqtt-client-wechat-miniprogram/pages/index/index.js
@@ -1,86 +1,141 @@
-import mqtt from '../../utils/mqtt'
+import mqtt from "../../utils/mqtt.min.js";
 
 Page({
   data: {
-    btnText: '连接',
     client: null,
-    host: 'broker.emqx.io:8084',
-    topic: 'testtopic/miniprogram',
-    msg: 'Hello! I am from WeChat miniprogram',
+    conenctBtnText: "连接",
+    host: "wx.emqxcloud.cn",
+    subTopic: "testtopic/miniprogram",
+    pubTopic: "testtopic/miniprogram",
+    pubMsg: "Hello! I am from WeChat miniprogram",
+    receivedMsg: "",
     mqttOptions: {
-      protocolVersion: 4, //MQTT连接协议版本
-      clientId: 'emqx_cloud_miniprogram',
-      clean: true,
-      password: '',
-      username: '',
-      reconnectPeriod: 1000, // 1000毫秒，两次重新连接之间的间隔
-      connectTimeout: 30 * 1000, // 1000毫秒，两次重新连接之间的间隔
-      resubscribe: true // 如果连接断开并重新连接，则会再次自动订阅已订阅的主题（默认true）
+      username: "test",
+      password: "test",
+      reconnectPeriod: 1000, // 1000毫秒，设置为 0 禁用自动重连，两次重新连接之间的间隔时间
+      connectTimeout: 30 * 1000, // 30秒，连接超时时间
+      // 更多参数请参阅 MQTT.js 官网文档：https://github.com/mqttjs/MQTT.js#mqttclientstreambuilder-options
+      // 更多 EMQ 相关 MQTT 使用教程可在 EMQ 官方博客中进行搜索：https://www.emqx.com/zh/blog
     },
   },
+
+  setValue(key, value) {
+    this.setData({
+      [key]: value,
+    });
+  },
   setHost(e) {
-    this.setData({
-      host: e.detail.value,
-    })
+    this.setValue("host", e.detail.value);
   },
-  setTopic(e) {
-    this.setData({
-      topic: e.detail.value,
-    })
+  setSubTopic(e) {
+    this.setValue("subTopic", e.detail.value);
   },
-  setMsg(e) {
-    this.setData({
-      msg: e.detail.value,
-    })
+  setPubTopic(e) {
+    this.setValue("pubTopic", e.detail.value);
   },
+  setPubMsg(e) {
+    this.setValue("pubMsg", e.detail.value);
+  },
+  setRecMsg(msg) {
+    this.setValue("receivedMsg", msg);
+  },
+
   connect() {
-    this.data.client = mqtt.connect(`wxs://${this.data.host}/mqtt`, this.data.mqttOptions)
-    this.data.client.on('connect', () => {
-      wx.showToast({
-        title: '连接成功'
-      })
-      this.setData({
-        btnText: '已连接',
-      })
-      this.data.client.on('message', (topic, payload) => {
-        wx.showModal({
-          content: `收到 Topic: ${topic}, Payload: ${payload}`,
-          showCancel: false,
+    // MQTT-WebSocket 统一使用 /path 作为连接路径，连接时需指明，但在 EMQX Cloud 部署上使用的路径为 /mqtt
+    // 因此不要忘了带上这个 /mqtt !!!
+    // 微信小程序中需要将 wss 协议写为 wxs，且由于微信小程序出于安全限制，不支持 ws 协议
+    try {
+      this.setValue("conenctBtnText", "连接中...");
+      const clientId = new Date().getTime();
+      this.data.client = mqtt.connect(`wxs://${this.data.host}:8084/mqtt`, {
+        ...this.data.mqttOptions,
+        clientId,
+      });
+
+      this.data.client.on("connect", () => {
+        wx.showToast({
+          title: "连接成功",
         });
-      })
-      this.data.client.on('error', (error) => {
-        console.log('error')
-      })
-      this.data.client.on('reconnect', () => {
-        console.log('Reconnecting...')
-      })
-      this.data.client.on('offline', (errr) => {
-        console.log('Offline')
-      })
-    })
+        this.setValue("conenctBtnText", "连接成功");
+
+        this.data.client.on("message", (topic, payload) => {
+          wx.showModal({
+            content: `收到消息 - Topic: ${topic}，Payload: ${payload}`,
+            showCancel: false,
+          });
+          const currMsg = this.data.receivedMsg ? `<br/>${payload}` : payload;
+          this.setValue("receivedMsg", this.data.receivedMsg.concat(currMsg));
+        });
+
+        this.data.client.on("error", (error) => {
+          this.setValue("conenctBtnText", "连接");
+          console.log("onError", error);
+        });
+
+        this.data.client.on("reconnect", () => {
+          this.setValue("conenctBtnText", "连接");
+          console.log("reconnecting...");
+        });
+
+        this.data.client.on("offline", () => {
+          this.setValue("conenctBtnText", "连接");
+          console.log("onOffline");
+        });
+        // 更多 MQTT.js 相关 API 请参阅 https://github.com/mqttjs/MQTT.js#api
+      });
+    } catch (error) {
+      this.setValue("conenctBtnText", "连接");
+      console.log("mqtt.connect error", error);
+    }
   },
-  disconnect() {
-    this.data.client.end()
-    this.setData({
-      btnText: '连接',
-    })
-    wx.showToast({
-      title: '断开连接成功'
-    })
-  },
+
   subscribe() {
-    this.data.client.subscribe(this.data.topic)
+    if (this.data.client) {
+      this.data.client.subscribe(this.data.subTopic);
+      wx.showModal({
+        content: `成功订阅主题：${this.data.subTopic}`,
+        showCancel: false,
+      });
+      return;
+    }
     wx.showToast({
-      title: `主题订阅成功`
-    })
+      title: "请先点击连接",
+      icon: "error",
+    });
   },
+
   unsubscribe() {
-    this.data.client.unsubscribe(this.data.topic)
+    if (this.data.client) {
+      this.data.client.unsubscribe(this.data.subTopic);
+      wx.showModal({
+        content: `成功取消订阅主题：${this.data.subTopic}`,
+        showCancel: false,
+      });
+      return;
+    }
     wx.showToast({
-      title: `取消订阅成功`
-    })
+      title: "请先点击连接",
+      icon: "error",
+    });
   },
+
   publish() {
-    this.data.client.publish(this.data.topic, this.data.msg)
+    if (this.data.client) {
+      this.data.client.publish(this.data.pubTopic, this.data.pubMsg);
+      return;
+    }
+    wx.showToast({
+      title: "请先点击连接",
+      icon: "error",
+    });
   },
-})
+
+  disconnect() {
+    this.data.client.end();
+    this.data.client = null;
+    this.setValue("conenctBtnText", "连接");
+    wx.showToast({
+      title: "成功断开连接",
+    });
+  },
+});
