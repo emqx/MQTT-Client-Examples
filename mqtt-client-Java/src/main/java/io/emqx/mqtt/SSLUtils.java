f31b21eaf27c6a504aeea51b74378155e5096e92
Zhiw <karlkb81@gmail.com>
2022-08-26 13:30:51 +0800

    feat(java-mqtt): add sub and pub samples of java

---
+++ mqtt-client-Java/src/main/java/io/emqx/mqtt/SSLUtils.java
@@ -0,0 +1,96 @@
+package io.emqx.mqtt;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+
+import java.io.*;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.Security;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+public class SSLUtils {
+
+    public static SSLSocketFactory getSingleSocketFactory(InputStream caCrtFileInputStream) throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+        X509Certificate caCert = null;
+
+        BufferedInputStream bis = new BufferedInputStream(caCrtFileInputStream);
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+        while (bis.available() > 0) {
+            caCert = (X509Certificate) cf.generateCertificate(bis);
+        }
+        KeyStore caKs = KeyStore.getInstance(KeyStore.getDefaultType());
+        caKs.load(null, null);
+        caKs.setCertificateEntry("cert-certificate", caCert);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(caKs);
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+        return sslContext.getSocketFactory();
+    }
+
+
+    public static SSLSocketFactory getSocketFactory(final String caCrtFile,
+                                                    final String crtFile, final String keyFile, final String password)
+            throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+
+        // load CA certificate
+        X509Certificate caCert = null;
+
+        FileInputStream fis = new FileInputStream(caCrtFile);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+        while (bis.available() > 0) {
+            caCert = (X509Certificate) cf.generateCertificate(bis);
+        }
+
+        // load client certificate
+        bis = new BufferedInputStream(new FileInputStream(crtFile));
+        X509Certificate cert = null;
+        while (bis.available() > 0) {
+            cert = (X509Certificate) cf.generateCertificate(bis);
+        }
+
+        // load client private key
+        PEMParser pemParser = new PEMParser(new FileReader(keyFile));
+        Object object = pemParser.readObject();
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+        KeyPair key = converter.getKeyPair((PEMKeyPair) object);
+        pemParser.close();
+
+        // CA certificate is used to authenticate server
+        KeyStore caKs = KeyStore.getInstance(KeyStore.getDefaultType());
+        caKs.load(null, null);
+        caKs.setCertificateEntry("ca-certificate", caCert);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+        tmf.init(caKs);
+
+        // client key and certificates are sent to server, so it can authenticate
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        ks.load(null, null);
+        ks.setCertificateEntry("certificate", cert);
+        ks.setKeyEntry("private-key", key.getPrivate(), password.toCharArray(),
+                new java.security.cert.Certificate[]{cert});
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory
+                .getDefaultAlgorithm());
+        kmf.init(ks, password.toCharArray());
+
+        // finally, create SSL socket factory
+        SSLContext context = SSLContext.getInstance("TLSv1.2");
+        context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+        return context.getSocketFactory();
+    }
+}
---
+++ mqtt-client-Java/src/main/java/io/emqx/mqtt/SubscribeSample.java
@@ -0,0 +1,50 @@
+package io.emqx.mqtt;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class SubscribeSample {
+    public static void main(String[] args) {
+
+        String broker = "tcp://broker.emqx.io:1883";
+        String topic = "mqtt/test";
+        String username = "emqx";
+        String password = "public";
+        String clientid = "subscribe_client";
+        int qos = 0;
+
+        try {
+            MqttClient client = new MqttClient(broker, clientid, new MemoryPersistence());
+            // 设置连接参数
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+            options.setUserName(username);
+            options.setPassword(password.toCharArray());
+            options.setConnectionTimeout(60);
+            options.setKeepAliveInterval(60);
+            // 设置回调
+            client.setCallback(new MqttCallback() {
+
+                public void connectionLost(Throwable cause) {
+                    System.out.println("connectionLost: " + cause.getMessage());
+                }
+
+                public void messageArrived(String topic, MqttMessage message) {
+                    System.out.println("topic: " + topic);
+                    System.out.println("Qos: " + message.getQos());
+                    System.out.println("message content: " + new String(message.getPayload()));
+
+                }
+
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    System.out.println("deliveryComplete---------" + token.isComplete());
+                }
+
+            });
+            client.connect(options);
+            client.subscribe(topic, qos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
