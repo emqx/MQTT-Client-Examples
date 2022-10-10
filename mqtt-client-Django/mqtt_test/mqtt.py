c95054ce500c71f344ee0a02d28d6c0d8616b053
Swilder-M <poxiaobbs@gmail.com>
2022-10-10 16:08:32 +0800

    Add Django example

---
+++ mqtt-client-Django/mqtt_test/mqtt.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+from django.conf import settings
+
+
+def on_connect(mqtt_client, userdata, flags, rc):
+    if rc == 0:
+        print('Connected successfully')
+        mqtt_client.subscribe('django/mqtt')
+    else:
+        print('Bad connection. Code:', rc)
+
+
+def on_message(mqtt_client, userdata, msg):
+    print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
+client.connect(
+    host=settings.MQTT_SERVER,
+    port=settings.MQTT_PORT,
+    keepalive=settings.MQTT_KEEPALIVE
+)
