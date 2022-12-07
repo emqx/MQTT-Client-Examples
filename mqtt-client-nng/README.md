fce6cca60f8f3fb9135ab341d778aee1f42d6791
alvin1221 <68314044+alvin1221@users.noreply.github.com>
2022-12-07 14:37:10 +0800

    Update project mqtt-client-nng (#62)
    * fix (nng-mqtt): update to latest callback functions

--- mqtt-client-nng/README.md
+++ mqtt-client-nng/README.md
@@ -1,17 +1,17 @@
-# nng-mqtt
+# NanoSDK
 
-> nng API：[libnng.3.adoc](https://github.com/nanomq/nng/blob/nng-mqtt/docs/man/libnng.3.adoc)
-
-### 安装nng库
+### 安装NanoSDK库
 
 ```shell
-git clone -b nng-mqtt https://github.com/nanomq/nng.git
-cd nng
+git clone https://github.com/emqx/NanoSDK
+cd NanoSDK
 mkdir build 
 cd build
 cmake -G Ninja ..
-# 若需支持TLS: 
+# 编译TLS: 
 # cmake -G Ninja -DNNG_ENABLE_TLS=ON ..
+# 编译SQLITE: 
+# cmake -G Ninja -DNNG_ENABLE_SQLITE=ON ..
 ninja
 ninja install
 ```
@@ -56,152 +56,65 @@ nng_mqtt_msg_alloc(&msg, 0);
 nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_CONNECT);
 nng_mqtt_msg_set_connect_keep_alive(msg, 60);
 nng_mqtt_msg_set_connect_clean_session(msg, true);
+nng_mqtt_msg_set_connect_proto_version(msg, opts->version);
 ```
+
+
 
 ### 创建并设置nng_dialer
 
-#### tcp模式
-
 ```c
-nng_mqtt_cb user_cb = {
-  .name            = "user_cb",
-  .on_connected    = connect_cb,
-  .on_disconnected = disconnect_cb,
-  .connect_arg     = &sock,
-  .disconn_arg     = "arg",
-};
 
-// url: "mqtt-tcp://127.0.0.1:1883"
-if ((rv = nng_dialer_create(&dialer, sock, url)) != 0) {
-    fatal("nng_dialer_create", rv);
+nng_mqtt_set_connect_cb(sock, connect_cb, &sock);
+nng_mqtt_set_disconnect_cb(sock, disconnect_cb, NULL);
+
+if ((rv = nng_dialer_create(&dialer, sock, opts->url)) != 0) {
+    fatal("nng_dialer_create: %s", nng_strerror(rv));
 }
+
+if (opts->enable_ssl) {
+    if ((rv = init_dialer_tls(dialer, opts->cacert, opts->cert,
+                opts->key, opts->keypass)) != 0) {
+        fatal("init_dialer_tls: %s", nng_strerror(rv));
+    }
+}
+
 nng_dialer_set_ptr(dialer, NNG_OPT_MQTT_CONNMSG, msg);
-nng_dialer_set_cb(dialer, &user_cb);
 nng_dialer_start(dialer, NNG_FLAG_NONBLOCK);
+
 ```
 
-#### tls模式
 
-##### 设置dialer tls
+### 订阅Topic
 
 ```c
-static int
-init_dialer_tls(nng_dialer d, const char *cacert, const char *cert,
-const char *key, const char *pass)
-{
-    nng_tls_config *cfg;
-    int             rv;
+ nng_mqtt_topic_qos subscriptions[] = {
+   {
+       .qos   = 1,
+       .topic = { 
+     .buf    = (uint8_t *) SUB_TOPIC1,
+           .length = strlen(SUB_TOPIC1), 
+    },
+   },
+   {
+       .qos   = 2,
+       .topic = { 
+     .buf    = (uint8_t *) SUB_TOPIC2,
+           .length = strlen(SUB_TOPIC2), 
+    },
+   },
+  };
 
-    if ((rv = nng_tls_config_alloc(&cfg, NNG_TLS_MODE_CLIENT)) != 0) {
-      return (rv);
-    }
+ nng_mqtt_cb_opt cb_opt = {
+  .sub_ack_cb = sub_callback,
+ };
 
-    if (cert != NULL && key != NULL) {
-      nng_tls_config_auth_mode(cfg, NNG_TLS_AUTH_MODE_REQUIRED);
-      if ((rv = nng_tls_config_own_cert(cfg, cert, key, pass)) !=
-          0) {
-        goto out;
-      }
-    } else {
-      nng_tls_config_auth_mode(cfg, NNG_TLS_AUTH_MODE_NONE);
-    }
+ nng_mqtt_client *client = nng_mqtt_client_alloc(&sock, &cb_opt, true);
+ nng_mqtt_subscribe_async(client, subscriptions,
+     sizeof(subscriptions) / sizeof(nng_mqtt_topic_qos), NULL);
 
-    if (cacert != NULL) {
-      if ((rv = nng_tls_config_ca_chain(cfg, cacert, NULL)) != 0) {
-        goto out;
-      }
-    }
-
-    rv = nng_dialer_setopt_ptr(d, NNG_OPT_TLS_CONFIG, cfg);
-
-out:
-    nng_tls_config_free(cfg);
-    return (rv);
-}	
 ```
 
-##### 创建dialer
-
-```c
-nng_mqtt_cb user_cb = {
-    .name            = "user_cb",
-    .on_connected    = connect_cb,
-    .on_disconnected = disconnect_cb,
-    .connect_arg     = &sock,
-    .disconn_arg     = "Args",
-};
-
-// url: "tls+mqtt-tcp://127.0.0.1:8883"
-if ((rv = nng_dialer_create(&dialer, sock, url)) != 0) {
-    fatal("nng_dialer_create", rv);
-}
-if ((rv = init_dialer_tls(dialer, ca, cert, key, pass)) != 0) {
-    fatal("init_dialer_tls", rv);
-}
-nng_dialer_set_ptr(dialer, NNG_OPT_MQTT_CONNMSG, msg);
-nng_dialer_set_cb(dialer, &user_cb);
-nng_dialer_start(dialer, NNG_FLAG_NONBLOCK);
-```
-
-### 在连接回调函数中进行topic订阅
-
-```c
-#define SUB_TOPIC1 "/nanomq/msg/1"
-#define SUB_TOPIC2 "/nanomq/msg/2"
-
-static void
-connect_cb(void *arg, nng_msg *msg)
-{
-    // Mqtt subscribe array of topic with qos
-    nng_mqtt_topic_qos topic_qos[] = {
-        { 
-            .qos   = 0,
-            .topic = {
-            .buf = (uint8_t *) SUB_TOPIC1,
-            .length = strlen(SUB_TOPIC1) 
-            } 
-        },
-        { 
-            .qos   = 1,
-            .topic = {
-            .buf = (uint8_t *) SUB_TOPIC2,
-            .length = strlen(SUB_TOPIC2) 
-            } 
-        }
-    };
-
-    size_t topic_qos_count = sizeof(topic_qos) / sizeof(nng_mqtt_topic_qos);
-    
-    nng_socket sock     = *(nng_socket *) arg;
-    uint8_t    ret_code = nng_mqtt_msg_get_connack_return_code(*msg*);
-	printf("%s: %s(%d)\n", __FUNCTION__,
-	    ret_code == 0 ? "connection established" : "connect failed", ret_code);
-    
-    nng_msg_free(msg);
-    msg = NULL;
-    
-    // Connected succeed
-    if (ret_code == 0) {
-        nng_mqtt_msg_alloc(&msg, 0);
-        nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_SUBSCRIBE);
-        nng_mqtt_msg_set_subscribe_topics(
-            msg, topic_qos, topic_qos_count);
-        // Send subscribe message
-        nng_sendmsg(sock, msg, NNG_FLAG_NONBLOCK);
-    }
-}
-```
-
-### 断开连接回调函数
-
-```c
-static void
-disconnect_cb(void *arg, nng_msg **msg*)
-{
-    (void) *arg*;
-    printf("%s\n",__FUNCTION__);
-}
-```
 
 ### 状态机回调函数
 
@@ -310,38 +223,48 @@ for (;;) {
 mkdir build
 cd build
 cmake -G Ninja ..
-# 若需支持TLS: 
+# 编译TLS: 
 # cmake -G Ninja -DNNG_ENABLE_TLS=ON ..
+# 编译SQLITE: 
+# cmake -G Ninja -DNNG_ENABLE_SQLITE=ON ..
 ninja
 ```
 
 ```shell
 mqtt_async: 
-        -u <broker url>
-        -n <number of works> (default: 32)
-        -s enable ssl/tls mode (default: disable)
-        -a <cafile path>
-        -c <cert file path>
-        -k <key file path>
-        -p <key password>
+    -h, --help    
+    --url           <url>    The url for mqtt broker 
+                    ('mqtt-tcp://host:port' or 
+                    'tls+mqtt-tcp://host:port')
+                    [default: mqtt-tcp://127.0.0.1:1883]
+    -n, --parallel  <number of works> (default: 32)
+    -v, --version   <mqtt version> (default: 4)
+    --sqlite        enable sqlite cache (default: false)
+    -s, --secure    enable ssl/tls mode (default: disable)
+    --cacert        <cafile path>
+    -E, --cert      <cert file path>
+    --key           <key file path>
+    --pey_pass      <key password>
 ```
 
 ```shell
 # connect broker via tcp
-./mqtt_async -u "mqtt-tcp://127.0.0.1:1883"
+./mqtt_async --url "mqtt-tcp://127.0.0.1:1883"
 ```
 
 ```shell
 # connect broker via tls
-./mqtt_async -u "tls+mqtt-tcp://127.0.0.1:8883" -s -a ca.crt -c server.crt -k server.key 
+./mqtt_async --url "tls+mqtt-tcp://127.0.0.1:8883" -s --cacert ca.crt --cert server.crt --key server.key 
 ```
 
-
+### NanoSDK实现案例
+[nanomq_cli](https://github.com/emqx/nanomq/tree/master/nanomq_cli)
 
 ### 完整代码工程
 
 [mqtt-client-nng](https://github.com/emqx/MQTT-Client-Examples/tree/master/mqtt-client-nng)
 
 
-### nng源码(nng-mqtt分支)
-[nng](https://github.com/nanomq/nng/tree/nng-mqtt)
+### NanoSDK源码
+[NanoSDK](https://github.com/emqx/NanoSDK)
+
--- mqtt-client-nng/mqtt_async.c
+++ mqtt-client-nng/mqtt_async.c
@@ -1,6 +1,6 @@
 #include <assert.h>
 #include <errno.h>
-#include <getopt.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,16 +11,195 @@
 #include <nng/mqtt/mqtt_client.h>
 #include <nng/nng.h>
 #include <nng/supplemental/util/platform.h>
-
-#ifdef NNG_SUPP_TLS
 #include <nng/supplemental/tls/tls.h>
+#include <nng/supplemental/util/options.h>
 
+static void
+fatal(const char *msg, ...)
+{
+	va_list ap;
+	va_start(ap, msg);
+	vfprintf(stderr, msg, ap);
+	va_end(ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+#define ASSERT_NULL(p, fmt, ...)           \
+	if ((p) != NULL) {                 \
+		fatal(fmt, ##__VA_ARGS__); \
+	}
+
+typedef struct {
+	size_t  parallel;
+	uint8_t version;
+	char *  url;
+	bool    enable_ssl;
+	char *  cacert;
+	size_t  cacert_len;
+	char *  cert;
+	size_t  cert_len;
+	char *  key;
+	size_t  key_len;
+	char *  keypass;
+	bool    enable_sqlite;
+} client_opts;
+
+enum options {
+	OPT_HELP = 1,
+	OPT_VERSION,
+	OPT_PARALLEL,
+	OPT_URL,
+	OPT_SECURE,
+	OPT_CACERT,
+	OPT_CERTFILE,
+	OPT_KEYFILE,
+	OPT_KEYPASS,
+	OPT_SQLITE,
+};
+
+static nng_optspec cmd_opts[] = {
+	{ .o_name = "help", .o_short = 'h', .o_val = OPT_HELP },
+	{ .o_name    = "parallel",
+	    .o_short = 'n',
+	    .o_val   = OPT_PARALLEL,
+	    .o_arg   = true },
+	{ .o_name    = "version",
+	    .o_short = 'V',
+	    .o_val   = OPT_VERSION,
+	    .o_arg   = true },
+	{ .o_name = "url", .o_val = OPT_URL, .o_arg = true },
+	{ .o_name = "sqlite", .o_val = OPT_SQLITE },
+	{ .o_name = "secure", .o_short = 's', .o_val = OPT_SECURE },
+	{ .o_name = "cacert", .o_val = OPT_CACERT, .o_arg = true },
+	{ .o_name = "key", .o_val = OPT_KEYFILE, .o_arg = true },
+	{ .o_name = "keypass", .o_val = OPT_KEYPASS, .o_arg = true },
+	{
+	    .o_name  = "cert",
+	    .o_short = 'E',
+	    .o_val   = OPT_CERTFILE,
+	    .o_arg   = true,
+	},
+};
+
+static void usage(void);
 static void loadfile(const char *path, void **datap, size_t *lenp);
 static int  init_dialer_tls(nng_dialer d, const char *cacert, const char *cert,
      const char *key, const char *pass);
+
+#if defined(NNG_SUPP_SQLITE)
+static int
+sqlite_config(
+    nng_socket *sock, nng_mqtt_sqlite_option *sqlite, uint8_t proto_ver)
+{
+	int rv;
+
+	// set sqlite option
+	nng_mqtt_set_sqlite_enable(sqlite, true);
+	nng_mqtt_set_sqlite_flush_threshold(sqlite, 50);
+	nng_mqtt_set_sqlite_max_rows(sqlite, 500);
+	nng_mqtt_set_sqlite_db_dir(sqlite, "/tmp/");
+
+	// init sqlite db
+	nng_mqtt_sqlite_db_init(sqlite, "mqtt_client.db", proto_ver);
+
+	// set sqlite option pointer to socket
+	return nng_socket_set_ptr(*sock, NNG_OPT_MQTT_SQLITE, sqlite);
+
+	return (0);
+}
 #endif
 
-static size_t nwork = 32;
+
+int
+client_parse_opts(int argc, char **argv, client_opts *opt)
+{
+	int    idx = 1;
+	char * arg;
+	int    val;
+	int    rv;
+
+	while ((rv = nng_opts_parse(argc, argv, cmd_opts, &val, &arg, &idx)) ==
+	    0) {
+		switch (val) {
+		case OPT_HELP:
+			usage();
+			exit(0);
+			break;
+		case OPT_PARALLEL:
+			opt->parallel = atol(arg);
+			break;
+		case OPT_VERSION:
+			opt->version = atol(arg);
+			break;
+		case OPT_URL:
+			ASSERT_NULL(opt->url,
+			    "URL (--url) may be specified "
+			    "only once.");
+			opt->url = nng_strdup(arg);
+			break;
+		case OPT_SQLITE:
+			opt->enable_sqlite = true;
+			break;
+		case OPT_SECURE:
+			opt->enable_ssl = true;
+			break;
+		case OPT_CACERT:
+			ASSERT_NULL(opt->cacert,
+			    "CA Certificate (--cacert) may be "
+			    "specified only once.");
+			loadfile(
+			    arg, (void **) &opt->cacert, &opt->cacert_len);
+			break;
+		case OPT_CERTFILE:
+			ASSERT_NULL(opt->cert,
+			    "Cert (--cert) may be specified "
+			    "only once.");
+			loadfile(arg, (void **) &opt->cert, &opt->cert_len);
+			break;
+		case OPT_KEYFILE:
+			ASSERT_NULL(opt->key,
+			    "Key (--key) may be specified only once.");
+			loadfile(arg, (void **) &opt->key, &opt->key_len);
+			break;
+		case OPT_KEYPASS:
+			ASSERT_NULL(opt->keypass,
+			    "Key Password (--keypass) may be specified only "
+			    "once.");
+			opt->keypass = nng_strdup(arg);
+			break;
+		}
+	}
+
+	switch (rv) {
+	case NNG_EINVAL:
+		fatal("Option %s is invalid.", argv[idx]);
+		usage();
+		break;
+	case NNG_EAMBIGUOUS:
+		fatal("Option %s is ambiguous (specify in full).", argv[idx]);
+		break;
+	case NNG_ENOARG:
+		fatal("Option %s requires argument.", argv[idx]);
+		break;
+	default:
+		break;
+	}
+
+	if (opt->url == NULL) {
+		opt->url = nng_strdup("mqtt-tcp://127.0.0.1:1883");
+	}
+
+	if (opt->version == 0) {
+		opt->version = MQTT_PROTOCOL_VERSION_v311;
+	}
+
+	if (opt->parallel == 0 ) {
+		opt->parallel = 32;
+	}
+
+	return rv;
+}
 
 struct work {
 	enum { INIT, RECV, WAIT, SEND } state;
@@ -32,12 +211,6 @@ struct work {
 #define SUB_TOPIC1 "/nanomq/msg/1"
 #define SUB_TOPIC2 "/nanomq/msg/2"
 #define SUB_TOPIC3 "/nanomq/msg/3"
-
-void
-fatal(const char *msg, int rv)
-{
-	fprintf(stderr, "%s: %s\n", msg, nng_strerror(rv));
-}
 
 void
 client_cb(void *arg)
@@ -55,7 +228,7 @@ client_cb(void *arg)
 
 	case RECV:
 		if ((rv = nng_aio_result(work->aio)) != 0) {
-			fatal("nng_recv_aio", rv);
+			fatal("nng_recv_aio: %s", nng_strerror(rv));
 			work->state = RECV;
 			nng_ctx_recv(work->ctx, work->aio);
 			break;
@@ -106,14 +279,14 @@ client_cb(void *arg)
 	case SEND:
 		if ((rv = nng_aio_result(work->aio)) != 0) {
 			nng_msg_free(work->msg);
-			fatal("nng_send_aio", rv);
+			fatal("nng_send_aio: %s", nng_strerror(rv));
 		}
 		work->state = RECV;
 		nng_ctx_recv(work->ctx, work->aio);
 		break;
 
 	default:
-		fatal("bad state!", NNG_ESTATE);
+		fatal("bad state: %s", nng_strerror(NNG_ESTATE));
 		break;
 	}
 }
@@ -125,80 +298,98 @@ alloc_work(nng_socket sock)
 	int          rv;
 
 	if ((w = nng_alloc(sizeof(*w))) == NULL) {
-		fatal("nng_alloc", NNG_ENOMEM);
+		fatal("nng_alloc: %s", nng_strerror( NNG_ENOMEM));
 	}
 	if ((rv = nng_aio_alloc(&w->aio, client_cb, w)) != 0) {
-		fatal("nng_aio_alloc", rv);
+		fatal("nng_aio_alloc: %s", nng_strerror(rv));
 	}
 	if ((rv = nng_ctx_open(&w->ctx, sock)) != 0) {
-		fatal("nng_ctx_open", rv);
+		fatal("nng_ctx_open: %s", nng_strerror(rv));
 	}
 	w->state = INIT;
 	return (w);
 }
 
 // Connack message callback function
-static void
-connect_cb(void *connect_arg, nng_msg *msg)
+void
+connect_cb(nng_pipe p, nng_pipe_ev ev, void *arg)
 {
-	// Mqtt subscribe array of topic with qos
-	nng_mqtt_topic_qos topic_qos[] = {
-		{ .qos     = 0,
-		    .topic = { .buf = (uint8_t *) SUB_TOPIC1,
-		        .length     = strlen(SUB_TOPIC1) } },
-		{ .qos     = 1,
-		    .topic = { .buf = (uint8_t *) SUB_TOPIC2,
-		        .length     = strlen(SUB_TOPIC2) } },
-		{ .qos     = 2,
-		    .topic = { .buf = (uint8_t *) SUB_TOPIC3,
-		        .length     = strlen(SUB_TOPIC3) } }
-	};
-
-	size_t topic_qos_count =
-	    sizeof(topic_qos) / sizeof(nng_mqtt_topic_qos);
-
-	nng_socket sock     = *(nng_socket *) connect_arg;
-	uint8_t    ret_code = nng_mqtt_msg_get_connack_return_code(msg);
-	printf("%s: %s(%d)\n", __FUNCTION__,
-	    ret_code == 0 ? "connection established" : "connect failed", ret_code);
-
-	nng_msg_free(msg);
-	msg = NULL;
-
-	if (ret_code == 0) {
-		// Connected succeed
-		nng_mqtt_msg_alloc(&msg, 0);
-		nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_SUBSCRIBE);
-		nng_mqtt_msg_set_subscribe_topics(
-		    msg, topic_qos, topic_qos_count);
-
-		// Send subscribe message
-		nng_sendmsg(sock, msg, NNG_FLAG_NONBLOCK);
-	}
+	int reason = 0;
+	// get connect reason
+	nng_pipe_get_int(p, NNG_OPT_MQTT_CONNECT_REASON, &reason);
+	// get property for MQTT V5
+	// property *prop;
+	// nng_pipe_get_ptr(p, NNG_OPT_MQTT_CONNECT_PROPERTY, &prop);
+	printf("%s: connected[%d]!\n", __FUNCTION__, reason);
 }
 
 // Disconnect message callback function
 static void
-disconnect_cb(void *disconn_arg, nng_msg *msg)
+disconnect_cb(nng_pipe p, nng_pipe_ev ev, void *arg)
 {
-	(void) disconn_arg;
-	printf("%s\n", __FUNCTION__);
+	int reason = 0;
+	// get connect reason
+	nng_pipe_get_int(p, NNG_OPT_MQTT_DISCONNECT_REASON, &reason);
+	// property *prop;
+	// nng_pipe_get_ptr(p, NNG_OPT_MQTT_DISCONNECT_PROPERTY, &prop);
+	printf("%s: disconnected!\n", __FUNCTION__);
+}
+
+static void
+sub_callback(void *arg)
+{
+	nng_mqtt_client *client = (nng_mqtt_client *) arg;
+	nng_aio *        aio    = client->sub_aio;
+	nng_msg *        msg    = nng_aio_get_msg(aio);
+
+	// Do not forget to judge if msg is NULL or not
+	if (msg != NULL) {
+		uint32_t count = 0;
+		uint8_t *codes;
+		codes = (uint8_t *) nng_mqtt_msg_get_suback_return_codes(
+		    msg, &count);
+
+		printf("%s: suback: ", __FUNCTION__);
+		for (uint32_t i = 0; i < count; i++) {
+			printf("[%d] ", codes[i]);
+		}
+		printf("\n");
+
+		nng_msg_free(msg);
+	}
 }
 
 int
-client(const char *url)
+client(client_opts *opts)
 {
 	nng_socket   sock;
 	nng_dialer   dialer;
-	struct work *works[nwork];
+	struct work *works[opts->parallel];
 	int          i;
 	int          rv;
 
-	if ((rv = nng_mqtt_client_open(&sock)) != 0) {
-		fatal("nng_socket", rv);
+	rv = opts->version == MQTT_PROTOCOL_VERSION_v5
+	    ? nng_mqttv5_client_open(&sock)
+	    : nng_mqtt_client_open(&sock);
+
+	printf("connecting to %s\n", opts->url);
+
+	if (rv != 0) {
+		fatal("nng_socket: %s", nng_strerror(rv));
 	}
 
-	for (i = 0; i < nwork; i++) {
+#if defined(NNG_SUPP_SQLITE)
+	nng_mqtt_sqlite_option *sqlite = NULL;
+	if (opts->enable_sqlite) {
+		if ((rv = nng_mqtt_alloc_sqlite_opt(&sqlite)) != 0) {
+			fatal(
+			    "nng_mqtt_alloc_sqlite_opt: %s", nng_strerror(rv));
+		}
+		sqlite_config(&sock, sqlite, opts->version);
+	}
+#endif
+
+	for (i = 0; i < opts->parallel; i++) {
 		works[i] = alloc_work(sock);
 	}
 
@@ -208,33 +399,64 @@ client(const char *url)
 	nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_CONNECT);
 	nng_mqtt_msg_set_connect_keep_alive(msg, 60);
 	nng_mqtt_msg_set_connect_clean_session(msg, true);
+	nng_mqtt_msg_set_connect_proto_version(msg, opts->version);
 
-	nng_mqtt_cb user_cb = {
-		.name            = "user_cb",
-		.on_connected    = connect_cb,
-		.on_disconnected = disconnect_cb,
-		.connect_arg     = &sock,
-		.disconn_arg     = "Args",
-	};
+	nng_mqtt_set_connect_cb(sock, connect_cb, &sock);
+	nng_mqtt_set_disconnect_cb(sock, disconnect_cb, NULL);
 
-	if ((rv = nng_dialer_create(&dialer, sock, url)) != 0) {
-		fatal("nng_dialer_create", rv);
+	if ((rv = nng_dialer_create(&dialer, sock, opts->url)) != 0) {
+		fatal("nng_dialer_create: %s", nng_strerror(rv));
+	}
+
+	if (opts->enable_ssl) {
+		if ((rv = init_dialer_tls(dialer, opts->cacert, opts->cert,
+		         opts->key, opts->keypass)) != 0) {
+			fatal("init_dialer_tls: %s", nng_strerror(rv));
+		}
 	}
 
 	nng_dialer_set_ptr(dialer, NNG_OPT_MQTT_CONNMSG, msg);
-	nng_dialer_set_cb(dialer, &user_cb);
 	nng_dialer_start(dialer, NNG_FLAG_NONBLOCK);
 
-	for (i = 0; i < nwork; i++) {
+	// subscribe to mqtt broker
+	nng_mqtt_topic_qos subscriptions[] = {
+			{
+			    .qos   = 1,
+			    .topic = { 
+					.buf    = (uint8_t *) SUB_TOPIC1,
+			        .length = strlen(SUB_TOPIC1), 
+				},
+			},
+			{
+			    .qos   = 2,
+			    .topic = { 
+					.buf    = (uint8_t *) SUB_TOPIC2,
+			        .length = strlen(SUB_TOPIC2), 
+				},
+			},
+		};
+
+	nng_mqtt_cb_opt cb_opt = {
+		.sub_ack_cb = sub_callback,
+	};
+
+	nng_mqtt_client *client = nng_mqtt_client_alloc(&sock, &cb_opt, true);
+	nng_mqtt_subscribe_async(client, subscriptions,
+	    sizeof(subscriptions) / sizeof(nng_mqtt_topic_qos), NULL);
+
+	for (i = 0; i < opts->parallel; i++) {
 		client_cb(works[i]);
 	}
 
 	for (;;) {
 		nng_msleep(3600000); // neither pause() nor sleep() portable
 	}
+
+#if defined(NNG_SUPP_SQLITE)
+	nng_mqtt_free_sqlite_opt(sqlite);
+#endif
 }
 
-#ifdef NNG_SUPP_TLS
 // This reads a file into memory.  Care is taken to ensure that
 // the buffer is one byte larger and contains a terminating
 // NUL. (Useful for key files and such.)
@@ -324,166 +546,43 @@ init_dialer_tls(nng_dialer d, const char *cacert, const char *cert,
 		}
 	}
 
-	rv = nng_dialer_setopt_ptr(d, NNG_OPT_TLS_CONFIG, cfg);
+	rv = nng_dialer_set_ptr(d, NNG_OPT_TLS_CONFIG, cfg);
 
 out:
 	nng_tls_config_free(cfg);
 	return (rv);
 }
 
-int
-tls_client(const char *url, const char *ca, const char *cert, const char *key,
-    const char *pass)
-{
-	nng_socket   sock;
-	nng_dialer   dialer;
-	struct work *works[nwork];
-	int          i;
-	int          rv;
-
-	if ((rv = nng_mqtt_client_open(&sock)) != 0) {
-		fatal("nng_socket", rv);
-	}
-
-	for (i = 0; i < nwork; i++) {
-		works[i] = alloc_work(sock);
-	}
-	// Mqtt connect message
-	nng_msg *msg;
-	nng_mqtt_msg_alloc(&msg, 0);
-	nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_CONNECT);
-	nng_mqtt_msg_set_connect_keep_alive(msg, 60);
-	nng_mqtt_msg_set_connect_clean_session(msg, true);
-
-	nng_mqtt_cb user_cb = {
-		.name            = "user_cb",
-		.on_connected    = connect_cb,
-		.on_disconnected = disconnect_cb,
-		.connect_arg     = &sock,
-		.disconn_arg     = "Args",
-	};
-
-	if ((rv = nng_dialer_create(&dialer, sock, url)) != 0) {
-		fatal("nng_dialer_create", rv);
-	}
-
-	if ((rv = init_dialer_tls(dialer, ca, cert, key, pass)) != 0) {
-		fatal("init_dialer_tls", rv);
-	}
-
-	nng_dialer_set_ptr(dialer, NNG_OPT_MQTT_CONNMSG, msg);
-	nng_dialer_set_cb(dialer, &user_cb);
-	nng_dialer_start(dialer, NNG_FLAG_NONBLOCK);
-
-	for (i = 0; i < nwork; i++) {
-		client_cb(works[i]);
-	}
-
-	for (;;) {
-		nng_msleep(3600000); // neither pause() nor sleep() portable
-	}
-}
-#endif
-
-void
+static void
 usage(void)
 {
 	printf("mqtt_async: \n");
-	printf("	-u <url> \n");
-	printf("	-n <number of works> (default: 32)\n");
-#ifdef NNG_SUPP_TLS
-	printf("	-s enable ssl/tls mode (default: disable)\n");
-	printf("	-a <cafile path>\n");
-	printf("	-c <cert file path>\n");
-	printf("	-k <key file path>\n");
-	printf("	-p <key password>\n");
-#endif
+	printf("    -h, --help    \n");
+	printf("    --url            <url>    The url for mqtt broker \n"
+	       "                     ('mqtt-tcp://host:port' or \n"
+	       "                     'tls+mqtt-tcp://host:port')\n"
+	       "                     [default: "
+	       "mqtt-tcp://127.0.0.1:1883]\n");
+	printf("    -n, --parallel   <number of works> (default: 32)\n");
+	printf("    -v, --version    <mqtt version> (default: 4)\n");
+	printf("    --sqlite         enable sqlite cache (default: false)\n");
+	printf("    -s, --secure     enable ssl/tls mode (default: disable)\n");
+	printf("    --cacert         <cafile path>\n");
+	printf("    -E, --cert       <cert file path>\n");
+	printf("    --key            <key file path>\n");
+	printf("    --pey_pass       <key password>\n");
 }
 
 int
 main(int argc, char **argv)
 {
 	int    rc;
-	char * path;
-	size_t file_len;
 
-	bool  enable_ssl = false;
-	char *url        = NULL;
-	char *cafile     = NULL;
-	char *cert       = NULL;
-	char *key        = NULL;
-	char *key_psw    = NULL;
+	client_opts opts = { 0 };
 
-	int   opt;
-	int   digit_optind  = 0;
-	int   option_index  = 0;
-	char *short_options = "u:n:sa:c:k:p:W;";
+	client_parse_opts(argc, argv, &opts);
 
-	static struct option long_options[] = {
-		{ "url", required_argument, NULL, 0 },
-		{ "nwork", no_argument, NULL, 'n' },
-		{ "ssl", no_argument, NULL, false },
-		{ "cafile", required_argument, NULL, 0 },
-		{ "cert", required_argument, NULL, 0 },
-		{ "key", required_argument, NULL, 0 },
-		{ "psw", required_argument, NULL, 0 },
-		{ "help", no_argument, NULL, 'h' },
-		{ NULL, 0, NULL, 0 },
-	};
-
-	while ((opt = getopt_long(argc, argv, short_options, long_options,
-	            &option_index)) != -1) {
-		switch (opt) {
-		case 0:
-			// TODO
-			break;
-		case '?':
-		case 'h':
-			usage();
-			exit(0);
-		case 'u':
-			url = argv[optind - 1];
-			break;
-		case 'n':
-			nwork = atoi(argv[optind - 1]);
-			break;
-		case 's':
-			enable_ssl = true;
-			break;
-#ifdef NNG_SUPP_TLS
-		case 'a':
-			path = argv[optind - 1];
-			loadfile(path, (void **) &cafile, &file_len);
-			break;
-		case 'c':
-			path = argv[optind - 1];
-			loadfile(path, (void **) &cert, &file_len);
-			break;
-		case 'k':
-			path = argv[optind - 1];
-			loadfile(path, (void **) &key, &file_len);
-			break;
-		case 'p':
-			key_psw = argv[optind - 1];
-			break;
-#endif
-		default:
-			fprintf(stderr, "invalid argument: '%c'\n", opt);
-			usage();
-			exit(1);
-		}
-	}
-
-	if (enable_ssl) {
-#ifdef NNG_SUPP_TLS
-		tls_client(url, cafile, cert, key, key_psw);
-#else
-		fprintf(stderr, "tls client: Not supported \n");
-#endif
-
-	} else {
-		client(url);
-	}
+	client(&opts);
 
 	return 0;
 }
