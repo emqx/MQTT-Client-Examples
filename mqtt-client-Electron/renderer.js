9877ea02db22cbf0781dde356112ad04eb393989
XL-YiBai <lei.xia@emqx.io>
2023-05-11 00:41:46 +0800

    feat(electron): support uploading CA, client-cert, and client-key

--- mqtt-client-Electron/renderer.js
+++ mqtt-client-Electron/renderer.js
@@ -26,13 +26,20 @@ const options = {
   // for more options, please refer to https://github.com/mqttjs/MQTT.js#mqttclientstreambuilder-options
 }
 
-const protocolSelect = document.getElementById('protocolSelect')
-const connectBtn = document.getElementById('connectBtn')
-const disconnectBtn = document.getElementById('disconnectBtn')
-const subBtn = document.getElementById('subBtn')
-const unsubBtn = document.getElementById('unsubBtn')
-const sendBtn = document.getElementById('sendBtn')
-const pathIpt = document.getElementById('pathIpt')
+const protocolSelect = document.getElementById('protocol-select')
+const connectBtn = document.getElementById('connect-btn')
+const disconnectBtn = document.getElementById('disconnect-btn')
+const subBtn = document.getElementById('sub-btn')
+const unsubBtn = document.getElementById('unsub-btn')
+const sendBtn = document.getElementById('send-btn')
+const pathIpt = document.getElementById('path-ipt')
+const fileArea = document.getElementById('file-area')
+const caIpt = document.getElementById('ca')
+const clientCertIpt = document.getElementById('client-cert')
+const clientKeyIpt = document.getElementById('clientKey')
+const caFileName = document.getElementById('caFileName')
+const clientCertFileName = document.getElementById('client-cert-file-name')
+const clientFileName = document.getElementById('client-key-file-name')
 
 protocolSelect.addEventListener('change', onProtocolChange)
 connectBtn.addEventListener('click', onConnect)
@@ -40,6 +47,29 @@ disconnectBtn.addEventListener('click', onDisconnect)
 subBtn.addEventListener('click', onSub)
 unsubBtn.addEventListener('click', onUnsub)
 sendBtn.addEventListener('click', onSend)
+caIpt.addEventListener('change', handleFileUpdate(caFileName, 'ca'))
+clientCertIpt.addEventListener(
+  'change',
+  handleFileUpdate(clientCertFileName, 'cert')
+)
+clientKeyIpt.addEventListener('change', handleFileUpdate(clientFileName, 'key'))
+
+// type => ca、cert、key     Representing server-ca, client-certificate, and client-key respectively
+function handleFileUpdate(fileName, type) {
+  return function (e) {
+    const file = e.target.files[0]
+    fileName.innerText = file.name
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        const fileContent = e.target.result
+        // Transfer TLS/SSL related files
+        options[type] = fileContent
+      }
+      reader.readAsText(file)
+    }
+  }
+}
 
 //  protocol->port    mqtt: 1883; mqtts: 8883; ws: 8083; wss: 8084
 function onProtocolChange() {
@@ -47,18 +77,22 @@ function onProtocolChange() {
     case 'mqtt':
       connection.port.value = 1883
       pathIpt.style.display = 'none'
+      fileArea.style.display = 'none'
       break
     case 'mqtts':
       connection.port.value = 8883
       pathIpt.style.display = 'none'
+      fileArea.style.display = 'block'
       break
     case 'ws':
       connection.port.value = 8083
       pathIpt.style.display = 'block'
+      fileArea.style.display = 'none'
       break
     case 'wss':
       connection.port.value = 8084
       pathIpt.style.display = 'block'
+      fileArea.style.display = 'block'
       break
     default:
       break
@@ -66,15 +100,18 @@ function onProtocolChange() {
 }
 
 // create MQTT connection
-function onConnect () {
-  const { protocol, host, port, clientId, username, password, path } = connection
-  
+function onConnect() {
+  const { protocol, host, port, clientId, username, password, path } =
+    connection
+
   let connectUrl = `${protocol.value}://${host.value}:${port.value}`
   if (protocol.value === 'ws' || protocol.value === 'wss') {
     connectUrl = connectUrl + path.value
   }
 
-  options.clientId = clientId.value || `mqttjs_${Math.random().toString(16).substr(2, 8)}`
+  options.clientId =
+    clientId.value ||
+    `emqx_electron_${Math.random().toString(16).substring(2, 10)}`
 
   /**
    * By default, EMQX allows clients to connect without authentication.
@@ -83,20 +120,20 @@ function onConnect () {
   options.username = username.value
   options.password = password.value
   console.log('connecting mqtt client')
-  
+
   /**
    * if protocol is "mqtt", connectUrl = "mqtt://broker.emqx.io:1883"
    * if protocol is "mqtts", connectUrl = "mqtts://broker.emqx.io:8883"
    * if protocol is "ws", connectUrl = "ws://broker.emqx.io:8083/mqtt"
    * if protocol is "wss", connectUrl = "wss://broker.emqx.io:8084/mqtt"
-   * 
+   *
    * /mqtt: MQTT-WebSocket uniformly uses /path as the connection path,
    * which should be specified when connecting, and the path used on EMQX is /mqtt.
-   * 
+   *
    * Note: Here we use the mqtts or wss protocol with TLS one-way authentication,
    * which only verifies the server's certificate and does not verify the client's certificate.
    * If you require two-way authentication, please use TLS two-way authentication.
-   * 
+   *
    * for more details about "mqtt.connect" method & options,
    * please refer to https://github.com/mqttjs/MQTT.js#mqttconnecturl-options
    */
@@ -130,7 +167,7 @@ function onConnect () {
 
 // disconnect
 // https://github.com/mqttjs/MQTT.js#mqttclientendforce-options-callback
-function onDisconnect () {
+function onDisconnect() {
   if (client.connected) {
     // https://github.com/mqttjs/MQTT.js#end
     client.end()
@@ -143,26 +180,30 @@ function onDisconnect () {
 
 // subscribe topic
 // https://github.com/mqttjs/MQTT.js#mqttclientsubscribetopictopic-arraytopic-object-options-callback
-function onSub () {
+function onSub() {
   if (client.connected) {
     // https://github.com/mqttjs/MQTT.js#qos
     const { topic, qos } = subscriber
-    client.subscribe(topic.value, { qos: parseInt(qos.value, 10) }, (error, res) => {
-       if (error) {
-         console.error('Subscribe error: ', error)
-       } else {
-         console.log('Subscribed: ', res)
-       }
-    })
+    client.subscribe(
+      topic.value,
+      { qos: parseInt(qos.value, 10) },
+      (error, res) => {
+        if (error) {
+          console.error('Subscribe error: ', error)
+        } else {
+          console.log('Subscribed: ', res)
+        }
+      }
+    )
   }
 }
 
 // unsubscribe topic
 // https://github.com/mqttjs/MQTT.js#mqttclientunsubscribetopictopic-array-options-callback
-function onUnsub () {
+function onUnsub() {
   if (client.connected) {
     const { topic } = subscriber
-    client.unsubscribe(topic.value, error => {
+    client.unsubscribe(topic.value, (error) => {
       if (error) {
         console.error('Unsubscribe error: ', error)
       } else {
@@ -174,12 +215,12 @@ function onUnsub () {
 
 // publish message
 // https://github.com/mqttjs/MQTT.js#mqttclientpublishtopic-message-options-callback
-function onSend () {
+function onSend() {
   if (client.connected) {
     const { topic, qos, payload } = publisher
     client.publish(topic.value, payload.value, {
       qos: parseInt(qos.value, 10),
-      retain: false
+      retain: false,
     })
   }
 }
