5efe1461f0880c75198fc5a3d071c601c1a35d95
Jaylin <64823539+JaylinYu@users.noreply.github.com>
2023-05-13 17:22:48 +0800

    Add New C# MQTT TLS example

---
+++ mqtt-client-Csharp/MqttNetTLSClient/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MqttClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // 自签名证书单向认证
+            // MqttClient.Client.TlsClientWithCA("E:/certs/cacert.pem").Wait();
+
+            // 双向认证
+            MqttClient.Client.TlsClientWithCert(@"E:/certs/cacert.pem", @"E:/certs/client.pfx","123456").Wait();
+        }
+    }
+}
