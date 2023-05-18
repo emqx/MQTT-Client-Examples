2b3dd675f722a60cc1f59c50c3c6ccd70b4d89ca
catgok <catgok@163.com>
2023-05-18 13:12:14 +0800

    feat(java): add tls/ssl config

--- mqtt-client-Java/src/main/java/io/emqx/mqtt/MqttSample.java
+++ mqtt-client-Java/src/main/java/io/emqx/mqtt/MqttSample.java
@@ -15,6 +15,24 @@ public class MqttSample {
         MqttConnectOptions connOpts = new MqttConnectOptions();
         connOpts.setUserName("emqx_user");
         connOpts.setPassword("emqx_password".toCharArray());
+
+        // ssl/tls config
+        try {
+            // broker = "ssl://broker.emqx.io:8883";
+
+            // one-way ssl/tls
+            // String caCrtFile = MqttSample.class.getResource("").getPath() + "./broker.emqx.io-ca.crt";
+            // connOpts.setSocketFactory(SSLUtils.getSingleSocketFactory(caCrtFile));
+
+            // two-way ssl/tls
+            // String caCrtFile = MqttSample.class.getResource("").getPath() + "./server-ca.crt";
+            // String crtFile = MqttSample.class.getResource("").getPath() + "./client.crt";
+            // String keyFile = MqttSample.class.getResource("").getPath() + "./client.key";
+            // connOpts.setSocketFactory(SSLUtils.getSocketFactory(caCrtFile, crtFile, keyFile, ""));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         try {
             MqttClient client = new MqttClient(broker, clientId, persistence);
             client.setCallback(new SampleCallback());
