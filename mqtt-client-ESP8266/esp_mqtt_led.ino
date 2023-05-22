1e5774cdebf9085cc6bab8b4154ba139ad8f084f
Mousse <zibuyu1995@gmail.com>
2023-05-22 18:12:40 +0800

    feat(esp8266): modify esp8266 control led code

--- mqtt-client-ESP8266/esp_mqtt_led.ino
+++ mqtt-client-ESP8266/esp_mqtt_led.ino
@@ -15,35 +15,46 @@ const char *mqtt_username = "emqx";
 const char *mqtt_password = "public";
 const int mqtt_port = 1883;
 
+bool ledState = false;
+
 WiFiClient espClient;
 PubSubClient client(espClient);
+
 
 void setup() {
     // Set software serial baud to 115200;
     Serial.begin(115200);
-    // connecting to a WiFi network
+    delay(1000); // Delay for stability
+
+    // Connecting to a WiFi network
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
-        Serial.println("Connecting to WiFi..");
+        Serial.println("Connecting to WiFi...");
     }
     Serial.println("Connected to the WiFi network");
-    //connecting to a mqtt broker
+
+    // Setting LED pin as output
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, LOW);  // Turn off the LED initially
+
+    // Connecting to an MQTT broker
     client.setServer(mqtt_broker, mqtt_port);
     client.setCallback(callback);
     while (!client.connected()) {
         String client_id = "esp8266-client-";
         client_id += String(WiFi.macAddress());
-        Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
         if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-            Serial.println("Public emqx mqtt broker connected");
+            Serial.println("Public EMQX MQTT broker connected");
         } else {
-            Serial.print("failed with state ");
+            Serial.print("Failed with state ");
             Serial.print(client.state());
             delay(2000);
         }
     }
-    // publish and subscribe
+
+    // Publish and subscribe
     client.publish(topic, "hello emqx");
     client.subscribe(topic);
 }
@@ -51,18 +62,25 @@ void setup() {
 void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print("Message arrived in topic: ");
     Serial.println(topic);
-    Serial.print("Message:");
+    Serial.print("Message: ");
     String message;
     for (int i = 0; i < length; i++) {
-        message = message + (char) payload[i];  // convert *byte to string
+        message += (char) payload[i];  // Convert *byte to string
     }
     Serial.print(message);
-    if (message == "on") { digitalWrite(LED, LOW); }   // LED on
-    if (message == "off") { digitalWrite(LED, HIGH); } // LED off
+    if (message == "on" && !ledState) {
+        digitalWrite(LED, HIGH);  // Turn on the LED
+        ledState = true;
+    }
+    if (message == "off" && ledState) {
+        digitalWrite(LED, LOW); // Turn off the LED
+        ledState = false;
+    }
     Serial.println();
     Serial.println("-----------------------");
 }
 
 void loop() {
     client.loop();
+    delay(100); // Delay for a short period in each loop iteration
 }
