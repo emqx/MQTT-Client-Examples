f890c12226ab571d3c56fbd4264e546164568882
wuzufeng <zufeng.wu@emqx.io>
2023-05-30 13:48:27 +0800

    refactor(mqtt-client-Csharp-MqttNet): refactor the project with MQTTNet 4.2

---
+++ mqtt-client-Csharp-MqttNet/pub-sub-tls/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+
+class Pub_sub_tcp
+{
+    static async Task Main(string[] args)
+    {
+        string broker = "broker.emqx.io";
+        int port = 8883;
+        string clientId = Guid.NewGuid().ToString();
+        string topic = "csharp/mqtt";
+        string username = "emqx";
+        string password = "public";
+
+        // Create a MQTT client factory
+        var factory = new MqttFactory();
+
+        // Create a MQTT client instance
+        var mqttClient = factory.CreateMqttClient();
+
+        // Create MQTT client options
+        var options = new MqttClientOptionsBuilder()
+            .WithTcpServer(broker, port) // MQTT broker address and port
+            .WithCredentials(username, password) // Set username and password
+            .WithClientId(clientId)
+            .WithCleanSession()
+            .WithTls(
+                o =>
+                {
+                    // The used public broker sometimes has invalid certificates. This sample accepts all
+                    // certificates. This should not be used in live environments.
+                    o.CertificateValidationHandler = _ => true;
+
+                    // The default value is determined by the OS. Set manually to force version.
+                    o.SslProtocol = SslProtocols.Tls12;
+                    var certificate = new X509Certificate("broker.emqx.io-ca.crt", "");
+                    o.Certificates = new List<X509Certificate> { certificate };
+                }
+            )
+            .Build();
+
+        // Connect to MQTT broker
+        var connectResult = await mqttClient.ConnectAsync(options);
+
+        if (connectResult.ResultCode == MqttClientConnectResultCode.Success)
+        {
+            Console.WriteLine("Connected to MQTT broker successfully.");
+
+            // Subscribe to a topic
+            await mqttClient.SubscribeAsync(topic);
+
+            // Callback function when a message is received
+            mqttClient.ApplicationMessageReceivedAsync += e =>
+            {
+                Console.WriteLine($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}");
+                return Task.CompletedTask;
+            };
+
+            // Publish a message 10 times
+            for (int i = 0; i < 10; i++)
+            {
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload($"Hello, MQTT! Message number {i}")
+                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                    .WithRetainFlag()
+                    .Build();
+
+                await mqttClient.PublishAsync(message);
+                await Task.Delay(1000); // Wait for 1 second
+            }
+
+            // Unsubscribe and disconnect
+            await mqttClient.UnsubscribeAsync(topic);
+            await mqttClient.DisconnectAsync();
+        }
+        else
+        {
+            Console.WriteLine($"Failed to connect to MQTT broker: {connectResult.ResultCode}");
+        }
+    }
+}
