6bf834fdae813446800c36451ac9530d23a37a45
Swilder-M <i@codming.com>
2025-02-05 16:44:20 +0800

    fix(django): update python version
    Signed-off-by: Swilder-M <i@codming.com>

--- mqtt-client-Django/django_mqtt/wsgi.py
+++ mqtt-client-Django/django_mqtt/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for mqtt_test project.
+WSGI config for django_mqtt project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mqtt_test.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_mqtt.settings')
 
 application = get_wsgi_application()
